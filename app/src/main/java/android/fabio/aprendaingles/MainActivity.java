b02378ea20a9bbc08225ea0c842f485e2c42e29c
fabioclaret <fabioclaret@gmail.com>
2026-02-01 12:10:32 -0300

    Abas funcionando

---
+++ app/src/main/java/android/fabio/aprendaingles/MainActivity.java
@@ -0,0 +1,53 @@
+package android.fabio.aprendaingles;
+
+import android.fabio.aprendaingles.fragments.BichosFragment;
+import android.fabio.aprendaingles.fragments.NumerosFragment;
+import android.fabio.aprendaingles.fragments.VogaisFragment;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager.widget.ViewPager;
+
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SmartTabLayout smartTabLayout;
+    private ViewPager viewPager;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        smartTabLayout = findViewById(R.id.viewpagertab);
+        viewPager = findViewById(R.id.viewpager);
+
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(),
+                FragmentPagerItems.with(this)
+                        .add("Animais" , BichosFragment.class)
+                        .add("Bichos"  , NumerosFragment.class)
+                        .add("Vogais"  , VogaisFragment.class)
+                        .create()
+        );
+
+        viewPager.setAdapter(adapter);
+        smartTabLayout.setViewPager(viewPager);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
